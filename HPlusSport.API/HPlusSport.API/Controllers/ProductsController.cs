00bf5c7a7ee7c8a4d7f52f9a0d15e01143092004
AIKursat <aynurkursat1999@hotmail.com>
2022-01-16 00:58:09 +0300

    Create the QueryParameter

--- HPlusSport.API/HPlusSport.API/Controllers/ProductsController.cs
+++ HPlusSport.API/HPlusSport.API/Controllers/ProductsController.cs
@@ -1,11 +1,17 @@
-﻿using HPlusSport.API.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HPlusSport.API.Classes;
+using HPlusSport.API.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace HPlusSport.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")] //[Route("api/[controller]")]
     [ApiController]
     public class ProductsController : ControllerBase
     {
@@ -14,9 +20,35 @@ namespace HPlusSport.API.Controllers
 
         public ProductsController(ShopContext context)
         {
-            _context = context;    
+            _context = context;
+
+            _context.Database.EnsureCreated();
         }
 
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts([FromQuery] QueryParameters queryParameters)
+        {
+            IQueryable<Product> products = _context.Products;
+
+          /*  products = products
+               .Skip()
+               .Take(); buraları incele
+                */ 
+
+            return Ok(await products.ToArrayAsync());
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var products = _context.Products.Where(x => x.Id == id);
+            if(products == null)
+            {
+                return NotFound("Id Not Found");
+            }
+            return Ok(products);
+        }
 
     }
-}
+
+    }
+
